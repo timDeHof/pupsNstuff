8610041e52b66edbd984ad2bef762a7aea95bb52
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-03-23 13:37:30 -0400

    seed demo

--- adapters.js
+++ adapters.js
@@ -2,6 +2,14 @@ const client = require('./client')
 
 const createPuppy = async (puppy) => {
   // Add a puppy (passed in) to our db
+  const { name, email, age } = puppy
+  await client.query(
+    `
+    INSERT INTO puppies (name, email, age)
+    VALUES ($1, $2, $3);
+  `,
+    [name, email, age]
+  )
 }
 
 module.exports = { createPuppy }
--- seed.js
+++ seed.js
@@ -3,15 +3,43 @@ const { createPuppy } = require('./adapters')
 const { puppies } = require('./seedData')
 
 // Drop Tables
+const dropTables = async () => {
+  console.log(`...dropping tables`)
+  await client.query(`
+        DROP TABLE IF EXISTS puppies;
+    `)
+  console.log(`...tables dropped`)
+}
 
 // Create Tables
-
+const createTables = async () => {
+  console.log(`...creating tables`)
+  await client.query(`
+        CREATE TABLE puppies (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR (255) NOT NULL,
+            email VARCHAR (255) UNIQUE NOT NULL,
+            "isCute" BOOLEAN DEFAULT true,
+            age INTEGER
+        );
+    `)
+  console.log(`...tables created`)
+}
 // Seed our Data
+const seedDb = async () => {
+  console.log(`...seeding the data`)
+  for (const puppy of puppies) {
+    await createPuppy(puppy)
+  }
+  console.log(`...information seeded`)
+}
 
 const rebuildDb = async () => {
   client.connect()
   try {
-    console.log(client)
+    await dropTables()
+    await createTables()
+    await seedDb()
   } catch (error) {
     console.error(error)
   } finally {
